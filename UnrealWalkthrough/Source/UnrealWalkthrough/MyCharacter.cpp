bd4e9c6077b12e2c18e2247e59aecb9b860456cb
MattB166 <matthewbrake2001@hotmail.co.uk>
2023-10-31 17:09:27 +0000

    UE4 push

---
+++ UnrealWalkthrough/Source/UnrealWalkthrough/MyCharacter.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCharacter.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	GLog->Log("Custom Message");
+	
+	
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	currentTime += DeltaTime;
+
+	if (currentTime > intervalTime)
+	{
+		UGameplayStatics::OpenLevel(GetWorld(), "NewMap");
+	}
+	
+	
+	GLog->Log("Time is" + FString::SanitizeFloat(DeltaTime));
+	
+	
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
